a1d5ee90ded18539bb963d38ffac73c4ab508a64
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2023-12-03 13:39:54 -0600

    Update pr.sh

--- pr.sh
+++ pr.sh
@@ -1,49 +1,30 @@
 #!/bin/bash
 
-echo "Opening a Pull Request"
-# Create PR
+# GitHub repository details
 REPO_OWNER="vidalgithub"
 REPO_NAME="gitops-argocd"
+
+# Branches for the pull request
 BASE_BRANCH="main"
 HEAD_BRANCH="feature"
-PR_TITLE="Updated Solar System Image"
-PR_BODY="Updated deployment specification with a new image version."
 
-#GITHUB_TOKEN=""
+# Pull request details
+PR_TITLE="Your Pull Request Title"
+PR_BODY="Description of your pull request"
 
-PR_RESPONSE=$(curl -L -X POST \
+# Create the pull request
+
+curl -L \
+  -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls \
-  -d '{
-    "title": "'"$PR_TITLE"'",
-    "body": "'"$PR_BODY"'",
-    "head": "'"$REPO_OWNER:$HEAD_BRANCH"'",
-    "base": "'"$BASE_BRANCH"'"
-  }')
+  -d '{"title":"Updated Solar System Image","body":"Updated deployment specification with a new image version.","head":"$HEAD_BRANCH","base":"$BASE_BRANCH"}'
 
-# Extract PR number from the response
-PR_NUMBER=$(echo "$PR_RESPONSE" | jq -r '.number')
-
-# Assuming you want to assign users "user1" and "user2"
-ASSIGN_USERNAMES=("kemgou")
-
-# Replace placeholders with actual values in the URL
-ISSUE_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER"
-
-curl -L -X PATCH \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  $ISSUE_URL \
-  -d '{
-    "title": "Found a bug",
-    "body": "I'\''m having a problem with this.",
-    "assignees": ['$(IFS=,; echo "${ASSIGN_USERNAMES[*]}")'],
-    "milestone": 1,
-    "state": "open",
-    "labels": ["bug"]
-  }'
-
-echo "Success"
+# Check the exit status
+if [ $? -eq 0 ]; then
+  echo "Pull Request created successfully."
+else
+  echo "Failed to create Pull Request."
+fi
