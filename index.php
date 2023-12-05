3da76288920fa05d6763f8e13559eb8cc4138e5f
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2023-12-05 03:48:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo '<!DOCTYPE html>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
 		#solar-system {
-		   background: url("https://github.com/vidalgithub/solar-system-9/blob/feature/images/solar-system-3.png")   center center;
+		   background: url("https://github.com/vidalgithub/solar-system-9/blob/feature/images/animated-solar-system.png")   center center;
            background-repeat: no-repeat;			  
 		   background-size: cover;
 		   content: "";
@@ -23,7 +23,7 @@ echo '<!DOCTYPE html>
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			background: url("https://github.com/vidalgithub/solar-system-9/blob/feature/images/background.png");
+			background: url("https://github.com/vidalgithub/solar-system-9/blob/feature/images/images/background.gif");
 		}
 		
 /* 		.shadow {
