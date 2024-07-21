6adac35f8c3bf490fd6cabd8b87a9a9258ee0c40
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2024-07-21 15:16:07 -0500

    Update prq.sh

--- prq.sh
+++ prq.sh
@@ -2,15 +2,15 @@
 
 # GitHub repository details
 REPO_OWNER="vidalgithub"
-REPO_NAME="your_repository"
+REPO_NAME="gitops-argocd"
 
 # Branches for the pull request
 BASE_BRANCH="main"
 HEAD_BRANCH="feature"
 
 # Pull request details
-PR_TITLE="Your Pull Request Title"
-PR_BODY="Description of your pull request"
+PR_TITLE="Update image"  #"Your Pull Request Title"
+PR_BODY="Update image"  #"Description of your pull request"
 
 # Create the pull request
 gh pr create -R $REPO_OWNER/$REPO_NAME --base $BASE_BRANCH --head $HEAD_BRANCH --title "$PR_TITLE" --body "$PR_BODY"
